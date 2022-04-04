80ffd743d0806d3c32a6a1118a6e4abec15af60f
Betty Anderson <101119141+bettyand@users.noreply.github.com>
2022-04-04 16:22:28 -0400

    finished

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 $(document).ready(function() {
     $("#submit").click(function(event) {
-        let name = $("#name").val();
-        let email = $("#email").val();
-        let phone = $("#phone").val();
+        let name = $("#name");
+        let email = $("#email");
+        let phone = $("#phone");
         let messageArea = $("#message-area");
         let required = [name, email, phone];
 
         for (let i = 0; i < required.length; i++) {
-            if (required[i] === "") {
+            if (required[i].val() === "") {
                 $("#message").html("Please Fill Out Required Fields").addClass("warning");
-                $(`#${required[i]}`).prev().addClass("warning");
+                required[i].prev().addClass("warning");
             } else {
-                $(`#${required[i]}`).prev().removeClass("warning");
+                required[i].prev().removeClass("warning");
+                $("#message").removeClass("warning");
             }
         }
 
-        if ($("label").class() !== "warning") {
+        if (!$("label").hasClass("warning")) {
             $("#form").remove();
             $("h2").html("<em>Thanks for your feedback!</em>");
         }
